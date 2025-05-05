fac0b1301f01d8f0d5fa4746f2499ea9f0343481
mf1bzz <guzemin@hotmail.com>
2025-05-05 22:13:48 +0800

    提不了一点

---
+++ arceos/labs/lab_allocator/src/bump.rs
@@ -0,0 +1,74 @@
+use allocator::{AllocResult, BaseAllocator, ByteAllocator};
+use core::alloc::Layout;
+use core::ptr::NonNull;
+use allocator::AllocError;
+pub struct BumpByteAllocator {
+    start: usize,
+    end: usize,
+    next: usize,
+    count: usize,
+}
+
+impl BumpByteAllocator {
+    pub const fn new() -> Self {
+        Self {
+            start: 0,
+            end: 0,
+            next: 0,
+            count: 0,
+        }
+    }
+}
+
+impl BaseAllocator for BumpByteAllocator {
+    fn init(&mut self, start: usize, size: usize) {
+        self.start = start;
+        self.end = start + size;
+        self.next = start;
+    }
+
+    fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
+        if self.end == start {
+            self.end = start+size;
+        } else if self.end == 0 {
+            self.start = start;
+            self.next = start;
+            self.end = start + size;
+        } else {
+            self.next = start;
+            self.end = start + size;
+        }
+        Ok(())
+    }
+}
+
+impl ByteAllocator for BumpByteAllocator {
+    fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
+        if self.next + layout.size() > self.end {
+            return Err(AllocError::NoMemory);
+        }
+        let ptr = NonNull::new(self.next as *mut u8).ok_or(AllocError::NoMemory)?;
+        self.next += layout.size();
+        self.count += 1;
+        Ok(ptr)
+    }
+
+    fn dealloc(&mut self, _pos: NonNull<u8>, _layout: Layout) {
+        self.count -= 1;
+        if self.count == 0 {
+            self.next = self.start;
+        }
+    }
+
+    fn total_bytes(&self) -> usize {
+        self.end - self.start
+    }
+
+    fn used_bytes(&self) -> usize {
+        self.next - self.start
+    }
+
+    fn available_bytes(&self) -> usize {
+        self.end - self.next
+    }
+}
--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -3,41 +3,88 @@
 #![no_std]
 #![allow(unused_variables)]
 
-use allocator::{BaseAllocator, ByteAllocator, AllocResult};
+mod bump;
+
+use allocator::{BaseAllocator, ByteAllocator, AllocResult, AllocError, SlabByteAllocator};
 use core::ptr::NonNull;
 use core::alloc::Layout;
+use bump::BumpByteAllocator;
 
-pub struct LabByteAllocator;
+pub struct LabByteAllocator {
+    next_tlsf: bool, // true: tlsf, false: other
+    will_dealloc: SlabByteAllocator,
+    never_dealloc: BumpByteAllocator,
+    add_memory_to_num: u8,
+}
 
 impl LabByteAllocator {
     pub const fn new() -> Self {
-        Self
+        Self {
+            next_tlsf: true,
+            will_dealloc: SlabByteAllocator::new(),
+            never_dealloc: BumpByteAllocator::new(),
+            add_memory_to_num: 0,
+        }
     }
 }
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        unimplemented!();
+        self.will_dealloc.init(start, size);
     }
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
-        unimplemented!();
+        // if self.will_dealloc.total_bytes() < MIN_SLAB_SIZE {
+        //     return self.will_dealloc.add_memory(start, size);
+        // }
+        match self.add_memory_to_num {
+            0 => {
+                self.will_dealloc.add_memory(start, size)
+            },
+            1 => {
+                self.never_dealloc.add_memory(start, size)
+            },
+            _ => Err(AllocError::InvalidParam),
+            
+        }
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
-        unimplemented!();
+        if !self.next_tlsf && layout.align() == 1 {
+            self.add_memory_to_num = 1;
+            let ptr = self.never_dealloc.alloc(layout)?;
+            self.next_tlsf = !self.next_tlsf;
+            if layout.size() >= 512 * 1024 {
+                self.next_tlsf = true;
+            }
+            Ok(ptr)
+        } else {
+            self.add_memory_to_num = 0;
+            let ptr = self.will_dealloc.alloc(layout)?;
+            if layout.align() == 1 {
+                self.next_tlsf = !self.next_tlsf;
+            }
+            if layout.size() >= 512 * 1024 {
+                self.next_tlsf = true;
+            }
+            Ok(ptr)
+        }
     }
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
-        unimplemented!();
+        self.will_dealloc.dealloc(pos, layout);
     }
     fn total_bytes(&self) -> usize {
-        unimplemented!();
+        match self.add_memory_to_num {
+            0 => self.will_dealloc.total_bytes(),
+            1 => self.never_dealloc.total_bytes(),
+            _ => 0,
+        }
     }
     fn used_bytes(&self) -> usize {
-        unimplemented!();
+        todo!()
     }
     fn available_bytes(&self) -> usize {
-        unimplemented!();
+        todo!()
     }
 }
